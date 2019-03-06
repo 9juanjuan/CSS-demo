975a5f135da9ee34279df95c1d5b0119069b3bcd
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-06 17:03:28 -0500

    Adds div with img tag at the top of html file. Adds classes to classlist which are attached to img classes to top div. Adds querySelector for top div element and toggles class change click events

--- scripts/index.js
+++ scripts/index.js
@@ -4,24 +4,36 @@ console.log("is this thing on? hello?")
 // A const is a constant varaible 
 //  which means that it cannot be reassigned.
 const targetElements = document.querySelectorAll("[data-target]") ;
-const background = document.querySelectorAll("body");
+const outputElement= document.querySelector("[data-output]")
+console.log(outputElement)
+
 function respondToClick(event) {
-    console.log('hello');
-    console.log('how are you?');
-    console.log('I am fine because I am a hamster.');
-
-    console.log(event.target.parentElement);
-
+    // console.log(event.target.getAttribute('src'))
+    // assigns src attribute to variable image 
+    const image = event.target.getAttribute('src')
+    // assign src of image to our data output element
+    outputElement.src = image
+    //  toggles hidden class onto our output element
+    outputElement.classList.toggle('hidden')
+    // toggles big class onto our target 
     event.target.parentElement.classList.toggle('big');
+
 
     // add the "big" class to the thing that got clicked"
 }
+function clickOnBig (event){
+    console.log('boop')
+    event.target.classList.toggle('hidden');
+}
 
-
+// function attachBigClickHandler(oneElement){
+//     oneElement.addEventListener("click", clickOnBig);
+// }
 function attachClickHandler(oneElement) {
     oneElement.addEventListener("click", respondToClick);
 }
 
+outputElement.addEventListener("click", clickOnBig);
 targetElements.forEach(attachClickHandler);
 // we should see an enlarged version of image that is
 // inside that .picture-frame.
